69b873a4ff3a56d447770fcfdba08a425b678ad7
tysenww <158533737+tysenww@users.noreply.github.com>
2024-12-08 03:59:23 -0600

    resolved some merge conflicts, still much to be desired. Known bug leaving bottom left maze cells with incorrect distances still in effect.

--- AbstractAlgorithmDesign/Main.cpp
+++ AbstractAlgorithmDesign/Main.cpp
@@ -22,10 +22,12 @@ struct mazeCell
 mazeCell maze[16][16];
 std::queue<std::array<int, 3>> floodfillQueue;
 
+//does a wall exist/is known for a cell in a specific direction
 bool isWall(int x, int y, int sideDirection) { //for sideDirection: 0: Left, 1: Top, 2: Right, 3: Bottom
     return (maze[x][y].wallConfig & (1 << (3 - sideDirection))) != 0;
 }
 
+//recursive function for floodfill
 void floodfillUtil(int x, int y, int curDistance) {
     floodfillQueue.pop(); //remove this element from the queue
 
@@ -38,9 +40,10 @@ void floodfillUtil(int x, int y, int curDistance) {
     if (maze[x][y].isGoal) { //goal is distance 0 from itself
         curDistance = 0;
     }
-    maze[x][y].floodfillChecked = true;
-    maze[x][y].toGoalDistance = curDistance;
+    maze[x][y].floodfillChecked = true; //this cell has now been checked
+    maze[x][y].toGoalDistance = curDistance; //set this cells distance
 
+    //add adjacent cells without a wall inbetween to the queue for processing
     if (!isWall(x, y, 0)) {
         floodfillQueue.push({x-1, y, curDistance+1});
     }
@@ -54,7 +57,7 @@ void floodfillUtil(int x, int y, int curDistance) {
         floodfillQueue.push({x, y-1, curDistance+1});
     }
 
-
+    //recursively process the queue until it is empty
     while (!floodfillQueue.empty()) {
         floodfillUtil(floodfillQueue.front()[0],floodfillQueue.front()[1],floodfillQueue.front()[2]);
     }
@@ -94,7 +97,7 @@ int main(int argc, char* argv[]) {
 
     floodfillUpdate();
 
-    //print maze cell distances
+    //print maze cell distances for testing/debugging
     for (int i = 0; i < 16; ++i) {
         for (int j = 0; j < 16; ++j) {
             std::cout << maze[i][j].toGoalDistance << " ";
@@ -102,6 +105,7 @@ int main(int argc, char* argv[]) {
         std::cout << std::endl;
     }
 
+    //the mouses run loop
     while (true) {
         if (!API::wallLeft()) {
             API::turnLeft();
